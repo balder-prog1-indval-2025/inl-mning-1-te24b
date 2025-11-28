ee2a1260704513c44b5f94143c9092d7c9cb5be2
oskarlifen <oli0311@student.skelleftea.se>
2025-11-28 09:32:06 +0100

    dumma jävla röda isak

--- app.ts
+++ app.ts
@@ -5,22 +5,22 @@
 let x =50  
 let y = 50
 
-var audio = new Audio('the-good,-the-bad-and-the-ugly-main-theme-made-with-Voicemod.mp3'); 
+var audio = new Audio('The Good the Bad and the Ugly.mp3'); 
 
 audio.play(); 
 
 audio.loop = true  
-
+let bakrund = await fetchImage("cards/marre.png")
 
 let å = 50
 
-
-update = () => {
+update = async () => {
     
     clear()
     
-    rectangle (W-150,å,50,H/5)
-    
+    ctx.drawImage(bakrund, 0, 0, W, H)
+    rectangle (W-150,å,50,H/5,"yellow")
+    let pinn = new Hitbox (W-150,å,50,H/5)
     if (keyboard.up && å > 0) {
         (å -= H/200)
 
@@ -29,8 +29,8 @@ update = () => {
         ( å += H/200)
 
     } 
-    rectangle (50,x,50,H/5)
-    
+    rectangle (50,x,50,H/5,"yellow")
+    let pinn2 = new Hitbox (50,x,50,H/5)
     if (keyboard.w && x > 0)  {
         (x -= H/200)
 
@@ -42,4 +42,7 @@ update = () => {
     
 } //oskar slut
 
+
+
+export { };
 
